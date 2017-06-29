73ead5a05ee3674622efce177bf0e4f6b05cbcb8
Dev <lodhad@gmail.com>
2017-06-29 10:13:15 -0700

    Testing performance

--- js/index.js
+++ js/index.js
@@ -1,85 +1,98 @@
 ;var to = null;
 var scrollToBody = null;
 (function init() {
-	$(document).ready(function() {
-		var danger = null;
-		$(document).ready(function ready() {
-			$.ajax({
-				dataType: "json",
-				url: "res/danger.json",
-				data: null,
-				success: function(success) {
-					danger = success;
-					danger.pic = danger.pic.replace(danger.esep, "/");
-					danger.email = danger.email.replace(danger.esep, "@");
+    var pperf = function pperf() {
+        if(!window.performance) {
+            console.info("window.Performance is not supported by your browser");
+        }
+        var perf = window.performance.timing;
+        var start = perf.navigationStart;
+        for(var k in perf) {
+            perf[k] -= start;
+        }
+        
+        console.info(perf);
+    };
 
-					setTimeout(function setVars() {
-						var pic = document.querySelectorAll("body > #home #details #picture")[0];
-						pic.style.backgroundImage = "url(../" + danger.pic + ")";
-					}, 100);
-				}
-			});
-		});
+    $(document).ready(function() {
+        var danger = null;
+        $(document).ready(function ready() {
+            $.ajax({
+                dataType: "json",
+                url: "res/danger.json",
+                data: null,
+                success: function(success) {
+                    danger = success;
+                    danger.pic = danger.pic.replace(danger.esep, "/");
+                    danger.email = danger.email.replace(danger.esep, "@");
 
-		var max = function max(a, b) {
-			if(a > b) return a;
-			return b;
-		};
+                    setTimeout(function setVars() {
+                        var pic = document.querySelectorAll("body > #home #details #picture")[0];
+                        pic.style.backgroundImage = "url(../" + danger.pic + ")";
+                    }, 100);
+                }
+            });
+        });
 
-		scrollToBody = function scrollToBody() {
-			$("html, body").animate({
-				scrollTop: max(0, $("#body").offset().top - 64)
-			});
-		};
+        var max = function max(a, b) {
+            if(a > b) return a;
+            return b;
+        };
 
-		to = function to(location, skip) {
-			if(location == "home")
-				history.pushState({}, '', window.location.protocol + '//' + window.location.host + window.location.pathname);
-			scrollElement = $("#" + location);
-			var update = function() {
-				$("#body").html("<h1>Loading Content...</h1>");
-				$.ajax({
-					dataType: "html",
-					url: "views/" + location + ".html",
-					data: null,
-					success: function(success) {
-						$("#body").html(success);
-						jQuery.getScript("js/" + location + ".js");
-						if(skip == null || skip) {
-							$("html, body").animate({
-								scrollTop: max(0, $("#body").offset().top - 64)
-							}, 1000);
-							history.pushState({}, '', window.location.protocol + '//' + window.location.host + window.location.pathname + "#" + location);
-						}
-					}
-				});
-			};
+        scrollToBody = function scrollToBody() {
+            $("html, body").animate({
+                scrollTop: max(0, $("#body").offset().top - 64)
+            });
+        };
+
+        to = function to(location, skip) {
+            if(location == "home")
+                history.pushState({}, '', window.location.protocol + '//' + window.location.host + window.location.pathname);
+            scrollElement = $("#" + location);
+            var update = function() {
+                $("#body").html("<h1>Loading Content...</h1>");
+                $.ajax({
+                    dataType: "html",
+                    url: "views/" + location + ".html",
+                    data: null,
+                    success: function(success) {
+                        $("#body").html(success);
+                        jQuery.getScript("js/" + location + ".js");
+                        if(skip == null || skip) {
+                            $("html, body").animate({
+                                scrollTop: max(0, $("#body").offset().top - 64)
+                            }, 1000);
+                            history.pushState({}, '', window.location.protocol + '//' + window.location.host + window.location.pathname + "#" + location);
+                        }
+                    }
+                });
+            };
 
 
-			if($(window).scrollTop() == 0) {
-				update();
-				return;
-			}
+            if($(window).scrollTop() == 0) {
+                update();
+                return;
+            }
 
-			$("html, body").animate({
-				scrollTop: 0
-			}, 500, function() {
-				if(location != "home")
-					update();
-			});
-		};
+            $("html, body").animate({
+                scrollTop: 0
+            }, 500, function() {
+                if(location != "home")
+                    update();
+            });
+        };
 
-		(function() {
-			var hash = window.location.hash;
-			if(!hash) {
-				to("projects", false);
-				return;
-			}
-			if(hash[0] == '#') hash = hash.substring(1, hash.length);
-			if(hash != "")
-				to(hash, true);
-			else
-				to("projects", false);
-		})();
-	});
+        (function() {
+            var hash = window.location.hash;
+            if(!hash) {
+                to("projects", false);
+                return;
+            }
+            if(hash[0] == '#') hash = hash.substring(1, hash.length);
+            if(hash != "")
+                to(hash, true);
+            else
+                to("projects", false);
+        })();
+    });
 })();
